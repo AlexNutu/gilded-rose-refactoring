df19e6f0ce7c463164e5712e455e1137ced89ec1
Alex Nutu <alexg.nutu@gmail.com>
2022-04-30 22:28:46 +0300

    Initial commit

---
+++ src/main/java/com/gildedrose/ItemType.java
@@ -0,0 +1,15 @@
+package com.gildedrose;
+
+public enum ItemType {
+
+    AGED_BRIE("Aged Brie");
+
+
+
+
+    public final String value;
+
+    ItemType(String value) {
+        this.value = value;
+    }
+}
