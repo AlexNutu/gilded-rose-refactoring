7740b15bea0c33a5b82464596ef058f85d8c7645
Alex Nutu <alexg.nutu@gmail.com>
2022-05-01 12:27:36 +0300

    C6 | Method extraction refactoring.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,55 +11,75 @@ class GildedRose {
 
     public void updateQuality() {
         for (final Item item : items) {
-            ItemType type = ItemType.fromString(item.name);
+            updateItemQuality(item);
+        }
+    }
 
-            if (!type.equals(ItemType.AGED_BRIE) && !type.equals(ItemType.BACKSTAGE_PASS)) {
+    private void updateItemQuality(Item item) {
+        ItemType type = ItemType.fromString(item.name);
 
+        updateQuality(item, type);
+
+        updateExpiration(item, type);
+
+        if (isExpired(item)) {
+
+            handleExpired(item, type);
+        }
+    }
+
+    private void updateQuality(Item item, ItemType type) {
+        if (!type.equals(ItemType.AGED_BRIE) && !type.equals(ItemType.BACKSTAGE_PASS)) {
+
+            if (item.quality > 0) {
+                if (!type.equals(ItemType.SULFURAS)) {
+                    degradeQuality(item);
+                }
+            }
+        }
+        else {
+            if (item.quality < 50) {
+                increaseQuality(item);
+
+                if (type.equals(ItemType.BACKSTAGE_PASS)) {
+                    if (item.sellIn < 11 && item.quality < 50) {
+                        increaseQuality(item);
+                    }
+
+                    if (item.sellIn < 6 && item.quality < 50) {
+                        increaseQuality(item);
+                    }
+                }
+            }
+        }
+    }
+
+    private void updateExpiration(Item item, ItemType type) {
+        if (!type.equals(ItemType.SULFURAS)) {
+            decreaseExpiry(item);
+        }
+    }
+
+    private boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private void handleExpired(Item item, ItemType type) {
+        if (!type.equals(ItemType.AGED_BRIE)) {
+            if (!type.equals(ItemType.BACKSTAGE_PASS)) {
                 if (item.quality > 0) {
                     if (!type.equals(ItemType.SULFURAS)) {
-                        degradeQuality(item);
+                       degradeQuality(item);
                     }
                 }
             }
             else {
-                if (item.quality < 50) {
-                    increaseQuality(item);
-
-                    if (type.equals(ItemType.BACKSTAGE_PASS)) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            increaseQuality(item);
-                        }
-
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            increaseQuality(item);
-                        }
-                    }
-                }
+                item.quality = 0;
             }
-
-            if (!type.equals(ItemType.SULFURAS)) {
-                decreaseExpiry(item);
-            }
-
-            if (item.sellIn < 0) {
-
-                if (!type.equals(ItemType.AGED_BRIE)) {
-                    if (!type.equals(ItemType.BACKSTAGE_PASS)) {
-                        if (item.quality > 0) {
-                            if (!type.equals(ItemType.SULFURAS)) {
-                               degradeQuality(item);
-                            }
-                        }
-                    }
-                    else {
-                        item.quality = 0;
-                    }
-                }
-                else {
-                    if (item.quality < 50) {
-                        increaseQuality(item);
-                    }
-                }
+        }
+        else {
+            if (item.quality < 50) {
+                increaseQuality(item);
             }
         }
     }
